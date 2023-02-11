83d6b8c31b57801bbb7eacd9ecdd8a560de6da3a
Alfredo Lucas <alfredoliu14@live.com>
2023-02-11 13:49:02 -0300

    adiciona hash na senha e salvo no banco de dados

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users",(req,res) => {
     res.send("Listagem de usuarios")
@@ -13,7 +14,17 @@ router.post("/users/create", (req,res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    res.json({email, password});
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        res.redirect("/");
+    });
 });
 
 module.exports = router;

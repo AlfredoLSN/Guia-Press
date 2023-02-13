a4323c15b45540a5b33954362a14ab05e4e7bf10
Alfredo Lucas <alfredoliu14@live.com>
2023-02-13 10:16:58 -0300

    implementa edicao de usuarios

--- users/UsersController.js
+++ users/UsersController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("./User");
 const bcrypt = require("bcryptjs");
+const Category = require("../categories/Category");
 
 router.get("/admin/users",(req,res) => {
     User.findAll().then(users => {
@@ -55,6 +56,46 @@ router.post("/users/delete", (req,res) => {
     }else{
       res.redirect("/admin/users");
     }
+})
+router.get("/admin/users/edit/:id", (req,res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+      res.redirect("/admin/users");
+    }
+
+    User.findByPk(id).then(user => {
+      if(user != undefined){
+        res.render("admin/users/edit",{user:user});
+      }else{
+        res.redirect("admin/users");
+      } 
+    }).catch(err => {
+        res.redirect("/admin/users");
+    })
+})
+router.post("/users/update",(req,res) => {
+     var id = req.body.id;
+     var email = req.body.email;
+     var password = req.body.password;
+     var oldEmail = req.body.oldEmail;
+     User.findOne({where: {email:email}}).then(user => {
+        if(user == undefined || user.email == oldEmail){
+          var salt = bcrypt.genSaltSync(10);
+          var hash = bcrypt.hashSync(password, salt);
+     
+          User.update({email: email, password: hash},{
+             where: {id: id}
+          }).then(() => {
+             res.redirect("/admin/users");
+          }).catch(err => {
+             res.redirect("/admin/users");
+          })
+        }else{
+          res.redirect("/admin/users/edit/" + id);
+        }
+     })
+
+     
 })
 
 module.exports = router;

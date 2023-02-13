17e049a75b3d7275d1d3292059ffe2c4aa482d1d
Alfredo Lucas <alfredoliu14@live.com>
2023-02-13 09:19:04 -0300

    impede email duplicado

--- users/UsersController.js
+++ users/UsersController.js
@@ -16,17 +16,28 @@ router.post("/users/create", (req,res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(password, salt);
+    User.findOne({where:{email: email}}).then(user => {
+        if(user == undefined){
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
 
-    User.create({
-        email: email,
-        password: hash
-    }).then(() => {
-        res.redirect("/");
-    }).catch((err) => {
-        res.redirect("/");
-    });
+            User.create({
+              email: email,
+              password: hash,
+            })
+              .then(() => {
+                res.redirect("/");
+              })
+              .catch((err) => {
+                res.redirect("/");
+              });
+        }else{
+            res.redirect("/admin/users/create")
+        }
+    })
+
+
+    
 });
 
 module.exports = router;
